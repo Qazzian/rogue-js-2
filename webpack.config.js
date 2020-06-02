1ab8b3d8cdf428624bfedae51305f51c48ba8027
Qazzian <Qazzian@gmail.com>
2020-06-02 17:11:50 +0100

    Install typesctipt and start basic config
    Note that this commit does not have a working build

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
-	entry: './src/index.js',
+	entry: './src/index.ts',
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist'),
 	},
 	module: {
 		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
 			{
 				test: /\.css$/,
 				use: [
@@ -24,6 +29,9 @@ module.exports = {
 				],
 			},
 		],
+	},
+	resolve: {
+		extensions: ['.ts', 'tsx', '.js'],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
