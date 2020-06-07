70b24e1b9741cf5b0e9d07505a49904303d4e44c
Qazzian <Qazzian@gmail.com>
2020-06-07 23:09:42 +0100

    Allow user to pick advanced heroquest map generator

--- src/RogueEngine/Game.js
+++ src/RogueEngine/Game.js
@@ -7,9 +7,12 @@ import { COLOURS } from '../PixelGameEngine/Colour';
 
 
 import TutorialMap from './mapGenerators/TutorialMap';
+import AdvancedHeroQuestGenerator from './mapGenerators/AdvancedHeroquestGenerator';
 
 function getMapGenerator(name) {
 	switch (name) {
+		case 'adv-heroquest':
+			return AdvancedHeroQuestGenerator;
 		default:
 		case 'tutorial':
 			return TutorialMap;
