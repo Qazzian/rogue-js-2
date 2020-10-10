9811867b9f605d1c0334175825e7c99f3aa65463
Qazzian <Qazzian@gmail.com>
2020-10-10 12:34:12 +0100

    TypeScript convert game entry point to tsx

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
-export default class Index {
-	readonly isBuilt: boolean;
-	constructor() {
-		this.isBuilt = true;
-	}
+import Game from './RogueEngine/Game';
 
-	alert() {
-		console.info('Has Built: ', this.isBuilt);
-	}
+import './index.css';
 
-}
 
-const myIndex = new Index();
-myIndex.alert();
+const screen = document.getElementById('game_screen');
+const stats = document.getElementById('stats');
+const game = new Game(screen, stats);
 
 
 
+window.addEventListener("keydown", (eventDescription) => {
+	game.handleKeyEvent(eventDescription);
+});
+
+// @ts-ignore
+document.getElementById('newMapButton').addEventListener('click', () => {
+	game.pause();
+	game.start();
+});
+// @ts-ignore
+document.getElementById('pause').addEventListener("click", () => game.pause());
+// @ts-ignore
+document.getElementById('unpause').addEventListener("click", () => game.unpause());
+// @ts-ignore
+document.getElementById('DebugFlag').addEventListener("change", (event:ChangeEvent) => {
+	const checked = event.currentTarget.checked;
+	game.setDebugFlag(checked);
+});
+game.start();
 
