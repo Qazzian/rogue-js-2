f34eb7f204148f4bb3315ed39267ec99f3057b8c
Qazzian <Qazzian@gmail.com>
2025-07-20 16:43:12 +0100

    auto fix some eslint errors

--- src/PixelGameEngine/fov/Edge.test.ts
+++ src/PixelGameEngine/fov/Edge.test.ts
@@ -1,29 +1,44 @@
-import Edge from './Edge';
+import Edge from "./Edge";
 
-describe('FOV functions', () => {
-	test('Edge Class', () => {
+describe("FOV functions", () => {
+	test("Edge Class", () => {
 		expect(Edge).toBeDefined();
 		const edge1 = new Edge(2, 5);
 		expect(edge1).toMatchObject({
-			x1: 2, x2: 2, y1: 5, y2: 5,
+			x1: 2,
+			x2: 2,
+			y1: 5,
+			y2: 5,
 		});
 
 		edge1.extend(1, 0);
 		expect(edge1).toMatchObject({
-			x1: 2, x2: 3, y1: 5, y2: 5,
+			x1: 2,
+			x2: 3,
+			y1: 5,
+			y2: 5,
 		});
 
 		const edge2 = new Edge(0, 0);
 		edge2.extend(0, -2);
 		expect(edge2).toMatchObject({
-			x1: 0, x2: 0, y1: 0, y2: -2,
+			x1: 0,
+			x2: 0,
+			y1: 0,
+			y2: -2,
 		});
 
 		edge2.extend(1, -1);
 		expect(edge2).toMatchObject({
-			x1: 0, x2: 1, y1: 0, y2: -3,
+			x1: 0,
+			x2: 1,
+			y1: 0,
+			y2: -3,
 		});
 
-		expect(edge2.getPoints()).toMatchObject([{x:0, y:0}, {x:1, y:-3}]);
+		expect(edge2.getPoints()).toMatchObject([
+			{ x: 0, y: 0 },
+			{ x: 1, y: -3 },
+		]);
 	});
 });
