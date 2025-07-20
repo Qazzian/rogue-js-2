58c520a54b57e2a58bfe28c43e9d418b42598dfe
Qazzian <Qazzian@gmail.com>
2025-07-20 16:42:44 +0100

    Add eslint and rules

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+
+export default tseslint.config([
+	{
+		files: ["**/*.{ts,mts,cts,jsx,tsx}"],
+		plugins: {
+			js,
+			"@typescript-eslint": tseslint,
+			prettier: prettierPlugin,
+		},
+		languageOptions: {globals: globals.browser},
+		extends: [
+			pluginReact.configs.flat.recommended,
+			prettierConfig,
+		],
+		rules: {
+			"@/no-unused-vars": "warn",
+			"no-console": "warn",
+			"semi": ["error", "always"],
+			"quotes": ["error", "double"],
+			"prettier/prettier": "error",
+		},
+	},
+]);
