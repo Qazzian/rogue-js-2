d8464d1891eb59045589ea857488fd5a68725f53
Qazzian <Qazzian@gmail.com>
2025-08-25 19:15:14 +0200

    Fix webpack config
    Removed alias for pixel-game-engine

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,54 @@
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 export default {
-	mode: 'development',
-	entry: './src/index.ts',
+	mode: "development",
+	entry: "./src/index.ts",
 	cache: {
-		type: 'filesystem',
-		buildDependencies: { config: [__filename] }
+		type: "filesystem",
+		buildDependencies: { config: [__filename] },
 	},
 	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
 	},
 	module: {
 		rules: [
 			{
 				test: /\.tsx?$/,
-				use: 'ts-loader',
+				use: "ts-loader",
 				exclude: /node_modules/,
 			},
 			{
 				enforce: "pre",
 				test: /\.js$/,
-				loader: "source-map-loader"
+				loader: "source-map-loader",
 			},
 			{
 				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader',
-				],
+				use: ["style-loader", "css-loader"],
 			},
 			{
 				test: /\.(png|svg|jpg|gif)$/,
-				use: [
-					'file-loader',
-				],
+				use: ["file-loader"],
 			},
 		],
 	},
 	resolve: {
-		extensions: ['.ts', '.tsx', '.js', '.json'],
-		alias: {
-			'@qazzian/pixel-game-engine': path.resolve(__dirname, '../pixel-game-engine/esm'),
-		}
+		extensions: [".ts", ".tsx", ".js", ".json"],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: './src/index.html',
+			template: "./src/index.html",
 		}),
 	],
-	devtool: 'inline-source-map',
+	devtool: "inline-source-map",
 	devServer: {
-		contentBase: './dist',
+		contentBase: "./dist",
 		port: 8081,
 	},
 };
